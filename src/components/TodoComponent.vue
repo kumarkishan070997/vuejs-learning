47db1f4e0c439868a58deca237ef0596fac60611
kishan.maharana <kishan.maharana@mail.vinove.com>
2022-12-04 23:39:02 +0530

    signup data local storage

--- src/components/TodoComponent.vue
+++ src/components/TodoComponent.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h1>TODOS</h1>
+        <p>
+            {{local_storage_signup}}
+        </p>
         <button class="btn-color" @click="fetchTodos">submit</button>
 
         <div>
@@ -8,25 +11,34 @@
                 {{post.title}}
             </div>
         </div>
-        <div>
+        <div class="form-group" :class="{ error: v$.user_detail.email.$errors.required }">
             <input type="text" name="name" v-model="user_detail.name">
-            <input type="email" name="email" v-model="user_detail.email">
+            <div v-if="v$.user_detail.email.$errors.required">code here</div>
+            <input type="email" name="email" v-model="v$.user_detail.email.$model" >
             <input type="number" name="phone" v-model="user_detail.phone">
             <input type="password" name="password" v-model="user_detail.password">
-            <button @click="submitData">submit form</button>
+            <button @click="signup">submit form</button>
         </div>
+
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import { useVuelidate } from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
 export default{
-    // name:'TodoComponent'
+        // name:'TodoComponent'
+    setup () {
+    return { v$: useVuelidate() }
+    },
     data(){
         return {
             api_url:"https://jsonplaceholder.typicode.com/posts",
             firebase_api:"https://vue-learn-237d9-default-rtdb.firebaseio.com/kishan.json",
+            // workstatus_signup=,
             all_data:[],
+            local_storage_signup: "",
             user_detail:{
                 id:1,
                 name:"",
@@ -54,8 +66,35 @@ export default{
                 // this.all_data = response.data
                 console.log(response)
             });
+        },
+        signup(){
+            axios.post(process.env.VUE_APP_WORKSTATUS_URL+'signUp',{
+                name:this.user_detail.name,
+                email:this.user_detail.email,
+                phone:this.user_detail.phone,
+                password:this.user_detail.password,
+                ip_address:"47.31.163.222",
+                phone_country_code_id:96,
+                source_url: "/auth/register"
+            }).then((response) => {
+                console.log(response);
+                localStorage.setItem("signup_response", JSON.stringify(response));
+                this.local_storage_signup = localStorage.getItem('signup_response');
+            }).catch((error)=>{
+                console.log(error);
+            });
         }
+    },
+    validations () {
+    return {
+      user_detail: {
+        email: { required, email },
+        name: { required },
+        phone: { required },
+        password: { required }
+      }
     }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,4 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import axios from 'axios';
 
+
+
 createApp(App).mount('#app')
+
+const axios = require('axios').default;
+axios.defaults.baseURL = process.env.VUE_APP_WORKSTATUS_URL;
+console.log(axios);
+console.log(axios.defaults.baseURL);
